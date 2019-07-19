ae3c82ff9ac2cd2edfa42dff24fa7c34cff51892
妖冶如你 <yaoyeruni@gmail.com>
2019-07-19 10:08:17 +0800

    rm comment(c++ cout风格日志前端)

--- src/LogStream.cc
+++ src/LogStream.cc
@@ -9,9 +9,9 @@ using namespace Angel;
 namespace Angel {
 
     // 默认打印所有级别的日志
-    int __loggerLevel = LogStream::DEBUG;
+    int __loggerLevel = Logger::DEBUG;
 
-    const char *levelStr[LogStream::LEVEL_NUMS] = {
+    const char *levelStr[Logger::LEVEL_NUMS] = {
         "DEBUG: ",
         "INFO:  ",
         "WARN:  ",
@@ -40,6 +40,7 @@ namespace Angel {
                     tm.tm_hour, tm.tm_min, tm.tm_sec, ms % 1000);
             log_last_second = seconds;
         } else {
+            // 在1s内只需要格式化ms部分
             snprintf(log_time_buf + 21, sizeof(log_time_buf) - 21,
                     "%03lld", ms % 1000);
         }
@@ -84,149 +85,10 @@ namespace Angel {
         *ptr = '\0';
         va_end(ap);
         __logger.writeToBuffer(log_output_buf, ptr - log_output_buf);
-        if (level == LogStream::FATAL)
+        if (level == Logger::FATAL)
             __logger.quit();
     }
 }
-
-// LogStream::LogStream(int level, const char *file, int line, const char *func)
-//     : _level(level),
-//     _file(file),
-//     _line(line),
-//     _func(func)
-// {
-//     *this << levelStr[level]
-//           << TimeStamp::timeStr(TimeStamp::LOCAL_TIME)
-//           << " " << _func << ": ";
-// }
-//
-// LogStream::~LogStream()
-// {
-//     *this << " - " << _file << ":" << _line
-//           << " - " << getThreadIdStr()
-//           << "\n";
-//     __logger.writeToBuffer(_buffer.peek(), _buffer.readable());
-//     if (_level == FATAL)
-//         __logger.quit();
-// }
-//
-// static const char digits[] = "9876543210123456789";
-// static const char* zero = digits + 9;
-//
-// namespace Angel {
-//
-//     __thread char _itoa_buf[64];
-// }
-//
-// // value -> str
-// template<typename T>
-// const char *convert(T value)
-// {
-//     T n = value;
-//     char *p = _itoa_buf;
-//
-//     do {
-//         int lsd = static_cast<int>(n % 10);
-//         n /= 10;
-//         *p++ = zero[lsd];
-//     } while (n);
-//     if (value < 0)
-//         *p++ = '-';
-//     *p = '\0';
-//
-//     std::reverse(_itoa_buf, p);
-//
-//     return _itoa_buf;
-// }
-//
-// template <typename V>
-// void numberToStr(LogStream& stream, V v)
-// {
-//     stream << convert(v);
-// }
-//
-// LogStream& LogStream::operator<<(const char *s)
-// {
-//     if (s)
-//         _buffer.append(s, std::strlen(s));
-//     else
-//         _buffer.append("(null)", 6);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(const std::string& s)
-// {
-//     _buffer.append(s.data(), s.size());
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(char v)
-// {
-//     _buffer.append(&v, 1);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(short v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(unsigned short v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(int v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(unsigned int v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(long v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(unsigned long v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(long long v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(unsigned long long v)
-// {
-//     numberToStr(*this, v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(float v)
-// {
-//     *this << static_cast<double>(v);
-//     return *this;
-// }
-//
-// LogStream& LogStream::operator<<(double v)
-// {
-//     char buf[32];
-//     snprintf(buf, sizeof(buf), "%.8g", v);
-//     *this << buf;
-//     return *this;
-// }
 
 void Angel::setLoggerLevel(int level)
 {
