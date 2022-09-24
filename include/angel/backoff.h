713341c52efb4a22890bc09f35c83736a53ced3d
妖冶如你 <yaoyeruni@gmail.com>
2022-09-24 22:09:28 +0800

    Add backoff.h

---
+++ include/angel/backoff.h
@@ -0,0 +1,29 @@
+#ifndef __ANGEL_BACKOFF_H
+#define __ANGEL_BACKOFF_H
+
+namespace angel {
+
+class ExponentialBackoff {
+public:
+    ExponentialBackoff() {  }
+    ExponentialBackoff(int init_interval, double multiplier, int max_retries)
+        : interval(init_interval), multiplier(multiplier), max_retries(max_retries) {  }
+    // Stop if return 0
+    int next_back_off()
+    {
+        if (max_retries == 0) return 0;
+        int backoff = interval;
+        interval *= multiplier;
+        --max_retries;
+        return backoff;
+    }
+private:
+    int interval = 200;
+    int max_retries = 6;
+    const double multiplier = 2.0;
+    // default: 200 400 800 1600 3200 6400
+};
+
+}
+
+#endif // __ANGEL_BACKOFF_H
