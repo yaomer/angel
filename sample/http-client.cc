4ca8be48c5f86afe02bc34ea9d7f988d4314c0dc
妖冶如你 <yaoyeruni@gmail.com>
2022-11-14 16:17:49 +0800

    Add sample/http-client.cc

---
+++ sample/http-client.cc
@@ -0,0 +1,32 @@
+#include <angel/httplib.h>
+
+#include <iostream>
+
+using namespace angel::httplib;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "./http-client [URL]\n");
+        return 1;
+    }
+
+    HttpClient client;
+    http_request request;
+
+    request.set_url(argv[1]).Get();
+
+    auto res = client.send(request).get();
+
+    if (res.err_code != ErrorCode::None) {
+        std::cout << res.err_str() << "\n";
+        return 1;
+    }
+
+    std::cout << res.status_code << " " << res.status_message << "\n";
+    for (auto& [k, v] : res.headers()) {
+        std::cout << k.key << ": " << v << "\n";
+    }
+    std::cout << "\n";
+    std::cout << res.body() << "\n";
+}
