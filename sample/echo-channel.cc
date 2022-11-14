b036d12314f56e536e46f22b6f084ae974e43223
妖冶如你 <yaoyeruni@gmail.com>
2022-11-14 16:46:56 +0800

    Encapsulate some operations with CMake function

--- sample/echo-channel.cc
+++ sample/echo-channel.cc
@@ -2,9 +2,14 @@
 
 #include <iostream>
 
+//
+// Single thread echo client
+//
 int main()
 {
     angel::evloop loop;
+
+    // Monitor socket
     angel::client cli(&loop, angel::inet_addr("127.0.0.1:8000"));
     cli.set_message_handler([](const angel::connection_ptr& conn, angel::buffer& buf){
             std::cout << buf.c_str() << "\n";
@@ -14,7 +19,7 @@ int main()
 
     char buf[1024];
     auto chl = std::make_shared<angel::channel>(&loop);
-    chl->set_fd(0);
+    chl->set_fd(0); // Monitor stdin
     chl->set_read_handler([&buf, &cli, &loop]{
             fgets(buf, sizeof(buf), stdin);
             if (cli.is_connected()) {
